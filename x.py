d1a609ab4dc92cbdcc7f5d174930f0106670096e
Pietro Francaviglia <pfrancaviglia@gmail.com>
2021-04-12 17:01:50 +0200

    Added Flask API

--- x.py
+++ x.py
@@ -3,14 +3,18 @@ import os
 import multiprocessing
 from elasticsearch import Elasticsearch
 from datetime import datetime
+import flask
+from flask import request
+from time import time
 
 crawlTime = 0
+processLimit = 5
 
 
 # load data on ElasticSearch
 def load(json_post):
     es = Elasticsearch([{'host': 'localhost', 'port': '9200'}])
-    es.index(index='prova_4chan', ignore=400, body=json_post)
+    es.index(index='4chan_index', ignore=400, body=json_post)
 
 
 # returns json data from a given page
@@ -42,7 +46,6 @@ def all_boards():
 
 # returns info about every post and reply on a certain page
 def page_posts(board, threads):
-    print("Crawling...")
     for i in range(0, len(threads)):
         endpoint = f"https://a.4cdn.org/{board}/thread/{threads[i]['no']}.json"
         data = get_json(endpoint)
@@ -54,7 +57,7 @@ def page_posts(board, threads):
                 data['posts'][j]['crawlTime'] = crawlTime
                 data['posts'][j]['board'] = board
 
-                if "tim" in data['posts'][j] and data['posts'][j]['ext'] is not ".swf":
+                if "tim" in data['posts'][j] and data['posts'][j]['ext'] != ".swf":
                     data['posts'][j]['img_link'] = f"https://i.4cdn.org/{board}/{data['posts'][j]['tim']}{data['posts'][j]['ext']}"
 
                 load(data['posts'][j])
@@ -66,7 +69,8 @@ def thread_list(board):
     data = get_json(endpoint)
 
     for i in range(0, len(data)):
-        print(f"Process {os.getpid()} working on board {board} at page {i+1}")
+        print(f"Process {os.getpid()} working on board {board} at page {i}")
+        print("Crawling...")
         page_posts(board, data[i]['threads'])
 
     print(f"End of board {board} analysis")
@@ -77,7 +81,7 @@ manager = multiprocessing.Manager()
 
 
 # retrieves the whole 4chan platform
-def crawl():
+def full_crawl():
     global crawlTime
 
     crawlTime = datetime.now()
@@ -91,7 +95,6 @@ def crawl():
         for k in range(0, len(boards)):
             pid = os.fork()
             if pid > 0:
-                print("This is the parent process {}".format(os.getpid()))
                 children.append(pid)
             else:
                 print("This is the child process {}".format(os.getpid()))
@@ -110,7 +113,7 @@ def crawl():
 
 
 # collects every post of a given channel
-def single_crawl(channel):
+def single_crawl(channel, max_process):
     global crawlTime
 
     crawlTime = datetime.now()
@@ -122,14 +125,22 @@ def single_crawl(channel):
 
     children = []
 
-    for i in range(0, len(data)):
+    process_num = len(data) if len(data) < max_process else max_process
+    crawl_num = int((len(data)-1)/max_process) + 1
+
+    for i in range(0, process_num):
         pid = os.fork()
         if pid > 0:
             print("This is the parent process {}".format(os.getpid()))
             children.append(pid)
         else:
-            print("This is the child process {}".format(os.getpid()))
+            print(f"Process {os.getpid()} working on board {channel} at page {i}")
             page_posts(channel, data[i]['threads'])
+            if len(data) > max_process:
+                for k in range(1, crawl_num):
+                    if max_process*k+i < len(data):
+                        print(f"Process {os.getpid()} working on board {channel} at page {max_process*k+i}")
+                        page_posts(channel, data[max_process*k+i]['threads'])
             print("Process {} exiting".format(os.getpid()))
             os._exit(0)
 
@@ -137,5 +148,32 @@ def single_crawl(channel):
         os.waitpid(pr, 0)
 
 
-# crawl()
-single_crawl('n')
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Hello world!</h1>"
+
+
+@app.route('/crawl', methods=['GET'])
+def crawl():
+    start = time()
+    if 'ch' in request.args:
+        if request.args['ch'] not in all_boards():
+            return f"<h1>Error</h1><p>Channel {request.args['ch']} not existing </p>"
+
+        if 'max' in request.args and int(request.args['max']) > 0:
+            single_crawl(request.args['ch'], int(request.args['max']))
+        else:
+            single_crawl(request.args['ch'], processLimit)
+
+        end = time()
+
+        return f"<h1>Success</h1><p>Crawl of channel {request.args['ch']} executed in {end - start} seconds</p>"
+
+    return "<h1>Error</h1><p>'ch' argument missing</p>"
+
+
+app.run()
